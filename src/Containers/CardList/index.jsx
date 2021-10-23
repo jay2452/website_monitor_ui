99e073310f45e382d21528db641db2756ff8c0fb
Jayant <jay2452@users.noreply.github.com>
2021-10-23 13:03:44 +0530

    Add files via upload

--- src/Containers/CardList/index.jsx
+++ src/Containers/CardList/index.jsx
@@ -1,7 +1,28 @@
-import { CircularProgress } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles((theme) => ({
+  card: {
+    margin: "10px 0",
+  },
+  url: {},
+  status: {},
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+}));
 
 const CardList = (props) => {
+  const classes = useStyle();
   const urlLists = useSelector((state) => state.urlList.data);
 
   if (urlLists === null) {
@@ -12,7 +33,31 @@ const CardList = (props) => {
     );
   }
 
-  return <div>List of URLS</div>;
+  return (
+    <div>
+      {urlLists &&
+        urlLists.map((item) => {
+          return (
+            <Card className={classes.card}>
+              {/* <CardMedia
+                component="img"
+                alt={item.url}
+                height="60"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              /> */}
+              <CardContent className={classes.cardContent}>
+                <div className={classes.url}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.url}
+                  </Typography>
+                </div>
+                <div className={classes.status}></div>
+              </CardContent>
+            </Card>
+          );
+        })}
+    </div>
+  );
 };
 
 export default CardList;
